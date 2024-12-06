e16e6969c3aa74fe55cf8f0044c85bcdb4366979
HaniMaanak <hmaanak@gmail.com>
2024-12-06 14:28:55 +0200

    added the js of the button submit in login page

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const passwordField = document.getElementById("password");
 const togglePassword = document.querySelector(".password-toggle-icon i");
+const link = document.getElementById("Submitlink"); // could not use the button way since submit was implemented in the html as <a>
 
 togglePassword.addEventListener("click", function () {
   if (passwordField.type === "password") {
@@ -11,4 +12,17 @@ togglePassword.addEventListener("click", function () {
     togglePassword.classList.remove("fa-eye-slash");
     togglePassword.classList.add("fa-eye");
   }
+});
+
+link.addEventListener('click', (event) => {
+    // Prevent default link behavior
+    event.preventDefault();
+
+    // Check if the password field is not empty
+    if (passwordField.value.trim() !== "") {
+        window.location.href = "index.html";
+    } else {
+        // Handle empty password field
+        alert("Please enter a password.");
+    }
 });
