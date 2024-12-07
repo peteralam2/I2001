059b887eec3e93c592ae9a5d4a2cd8d63f537ff9
peteralam2 <peteralam2004@gmail.com>
2024-12-07 18:04:18 +0200

    created a database for the login page and a homepage for future uses

---
+++ login/homepage.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHgXFgXvA9rJhQH-3wYMRvy2AFzGh8G_k",
+
+    authDomain: "project-48091.firebaseapp.com",
+
+    projectId: "project-48091",
+
+    storageBucket: "project-48091.firebasestorage.app",
+
+    messagingSenderId: "39347073069",
+
+    appId: "1:39347073069:web:7140b4bd3e6e9091fb48a4",
+
+    measurementId: "G-GF6GMK2CDN",
+  };
+ 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserFName').innerText=userData.firstName;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserLName').innerText=userData.lastName;
+
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document");
+        })
+    }
+    else{
+        console.log("User Id not Found in Local storage")
+    }
+  })
+
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='login.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })
