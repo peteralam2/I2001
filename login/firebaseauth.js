f8732bd5498ddaf6794c9098e94bb76f3e9273a5
peteralam2 <peteralam2004@gmail.com>
2024-12-22 19:59:32 +0200

    added a profile that fetches user info(name/email) and deleted the test login href

--- login/firebaseauth.js
+++ login/firebaseauth.js
@@ -85,7 +85,7 @@
         showMessage('login is successful', 'signInMessage');
         const user=userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='homepage.html';
+        window.location.href='/main/index.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
